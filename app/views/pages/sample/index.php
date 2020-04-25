1256df1610a4705571b49c053a3465d99ca8f044
frostygum <4nthonius.juan5@gmail.com>
2020-04-25 21:36:46 +0700

    css input update

--- app/views/pages/sample/index.php
+++ app/views/pages/sample/index.php
@@ -5,14 +5,13 @@
 <div class="container">
     <div class="card shadow">
         <div class="card-header">
-            <h6 class="m-0">Buttons</h6>
+            <h6 class="m-0">Button</h6>
         </div>
         <div class="card-content">
-            <span>Normal Buttons</span>
+            <span>Normal Button</span>
             <div class="display-grid grid-col-4 grid-g-1 mb-1">
                 <button class="btn">default</button>
                 <button class="btn btn-primary">primary</button>
-                <button class="btn btn-secondary">secondary</button>
                 <button class="btn btn-success">success</button>
                 <button class="btn btn-warning">warning</button>
                 <button class="btn btn-danger">danger</button>
@@ -20,23 +19,47 @@
                 <button class="btn btn-dark">dark</button>
             </div>
             
-            <span>Large Buttons</span>
-            <div class="display-grid grid-col-4 grid-g-1">
+            <span>Large Button</span>
+            <div class="display-grid grid-col-4 grid-g-1 mb-1">
                 <button class="btn btn-lg">default</button>
                 <button class="btn btn-lg btn-primary">primary</button>
-                <button class="btn btn-lg btn-secondary">secondary</button>
                 <button class="btn btn-lg btn-success">success</button>
                 <button class="btn btn-lg btn-warning">warning</button>
                 <button class="btn btn-lg btn-danger">danger</button>
                 <button class="btn btn-lg btn-info">info</button>
                 <button class="btn btn-lg btn-dark">dark</button>
             </div>
+
+            <span>Disabled Button</span>
+            <div class="display-grid grid-col-4 grid-g-1">
+                <button class="btn" disabled>default</button>
+                <button class="btn btn-primary" disabled>primary</button>
+                <button class="btn btn-success" disabled>success</button>
+                <button class="btn btn-warning" disabled>warning</button>
+                <button class="btn btn-danger" disabled>danger</button>
+                <button class="btn btn-info" disabled>info</button>
+                <button class="btn btn-dark" disabled>dark</button>
+            </div>
         </div>
     </div>
 
     <div class="card shadow">
         <div class="card-header">
-            <h6 class="m-0">Headings</h6>
+            <h6 class="m-0">Input</h6>
+        </div>
+        <div class="card-content">
+            <div class="display-grid grid-col-4 grid-g-1">
+                <input type="text" class="input" placeholder="default" />
+                <input type="text" class="input error" placeholder="error" />
+                <input type="text" class="input success" placeholder="success" />
+                <input type="text" class="input disabled" disabled placeholder="disabled" />
+            </div>
+        </div>
+    </div>
+
+    <div class="card shadow">
+        <div class="card-header">
+            <h6 class="m-0">Typography</h6>
         </div>
         <div class="card-content">
             <h1>Heading 1</h1>
@@ -45,6 +68,7 @@
             <h4>Heading 4</h4>
             <h5>Heading 5</h5>
             <h6>Heading 6</h6>
+            <p>This is a sample of a paragraph</p>
         </div>
     </div>
 </div>
--- app/views/pages/test/test.php
+++
@@ -1,9 +0,0 @@
-<?= $this::add_template('header') ?>
-
-<h1>ini test</h1>
-
-<p>
-    <?= $angka ?>
-</p>
-
-<?= $this::add_template('footer') ?>

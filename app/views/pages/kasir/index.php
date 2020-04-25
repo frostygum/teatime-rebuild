213daa07dfcd4979ec7afa62f85543ac97b75399
FrostyGum <4nthonius.juan5@gmail.com>
2020-04-25 22:52:32 +0700

    css update

--- app/views/pages/kasir/index.php
+++ app/views/pages/kasir/index.php
@@ -1,5 +1,7 @@
-<h1>Halaman Kasir</h1>
-
-<p>
-    <?= $title ?>
-</p>
+<div class="container">
+    <div class="card px-2 py-2 bg-teal text-light shadow" style="min-height: 10rem">
+        <div class="card-body">
+            test
+        </div>
+    </div>
+</div>

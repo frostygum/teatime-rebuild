772c876259981fceea37987326ba620c3268f98c
FrostyGum <4nthonius.juan5@gmail.com>
2020-05-08 16:08:18 +0700

    Kasir (#44)
    * router update
    
    * manager routing update

--- app/helpers/Router.php
+++ app/helpers/Router.php
@@ -94,6 +94,10 @@ class Router
             $base_url = $base_url[0];
             $url = str_replace('/' . $base_url, '', $url);
 
+            if(strstr($url, '?', true)) {
+                $url = strstr($url, '?', true);
+            }
+
             if ($url == '') {
                 $url = '/';
             }

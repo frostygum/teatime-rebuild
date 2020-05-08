7083ea41f5bf1aa798f38dc36cd8970a7ac328b7
FrostyGum <4nthonius.juan5@gmail.com>
2020-05-08 16:52:45 +0700

    update manager page (#48)
    * router update
    
    * manager routing update
    
    * kasir update
    
    * route fix
    
    * styling manager page

--- app/views/pages/manager/dashboard.php
+++ app/views/pages/manager/dashboard.php
@@ -5,26 +5,38 @@
     ?>
 
 
-    <div class="container">
+    <div class="container mt-4">
         
-        <?= isset($user_information) ? $user_information['username'] : '' ?>
-        <div class="card px-2 py-2 bg-teal text-light shadow" style="min-height: 10rem">
-            <div class="card-body">
-                isi
-                <div class="side-navbar">
-                <a href="" class="side-navbar-tab active">Dashboard</a>
-                <a href="" class="side-navbar-tab">Data</a>
-                <a href="" class="side-navbar-tab">Ranking</a>
+        <div class="card shadow display-flex" style="height: 70vh">
+            <!-- KIRI -->
+            <div class="bg-dark text-light p-2" style="width: 20rem; border-radius: var(--border-radius) 0 0 var(--border-radius)">
+                <div class="p-2">
+                    <h6>Dashboard</h6>
+                </div>
+                <div class="p-2">
+                    <h6>Data</h6>
+                </div>
+                <div class="p-2">
+                    <h6>Ranking</h6>
                 </div>
             </div>
-            <div class="card ">
-            banyak transaksi harian 
-            </div>
-            <div class="card ">
-            banyak pemasukan harian
-            </div>
-            <div class="card ">
-            banyak cup terjual 
+
+            <!-- KANAN -->
+            <div class="p-2" style="width: 100%;">
+                <div class="display-grid grid-col-3 grid-g-2">
+                    <div class="card bg-red shadow p-2 text-center text-light">
+                        <h4>100</h4>
+                        <p>Total Cups</p>
+                    </div>
+                    <div class="card bg-red shadow p-2 text-center text-light">
+                        <h4>100</h4>
+                        <p>Total Cups</p>
+                    </div>
+                    <div class="card bg-red shadow p-2 text-center text-light">
+                        <h4>100</h4>
+                        <p>Total Cups</p>
+                    </div>
+                </div>
             </div>
         </div>
 

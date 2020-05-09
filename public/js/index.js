73218c7f3ed7398d20a9fe92d536fb51630cacd6
frostygum <4nthonius.juan5@gmail.com>
2020-05-09 15:00:48 +0700

    aplying filter and search for selcet menu

--- public/js/index.js
+++ public/js/index.js
@@ -3,19 +3,6 @@ function toggleDropdown(targetId) {
     document.getElementById(targetId).classList.toggle("show-dropdown");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches('button')) {
-        let dropdown = document.getElementsByClassName("dropdown-content");
-        let i;
-        for (i = 0; i < dropdown.length; i++) {
-            let openDropdown = dropdown[i];
-            if (openDropdown.classList.contains('show-dropdown')) {
-                openDropdown.classList.remove('show-dropdown');
-            }
-        }
-    }
-}
-
 // !ALERT
 function toggleAlert(targetId) {
     let alertEvent = new CustomEvent('show-alert', {detail: targetId});
@@ -23,31 +10,9 @@ function toggleAlert(targetId) {
     window.dispatchEvent(alertEvent);
 }
 
-window.addEventListener('show-alert', function(event) {
-    let openAlert = document.getElementById(event.detail);
-    if(openAlert.classList.contains('show-alert')) {
-        setTimeout(function() {
-            document.getElementById(event.detail).classList.remove("show-alert");
-        }, 4000);
-    }
-});
-
 // !MODAL
 function toggleModal(targetId) {
     document.getElementById(targetId).classList.toggle('show-modal');
-}
-
-window.onclick = function(event) {
-    if (!event.target.matches('button') && event.target.matches('.modal-wrapper')) {
-        let modal = document.getElementsByClassName("modal");
-        let i;
-        for (i = 0; i < modal.length; i++) {
-            let openModal = modal[i];
-            if (openModal.classList.contains('show-modal')) {
-                openModal.classList.remove('show-modal');
-            }
-        }
-    }
 }
 
 // !CUSTOM SELECT 
@@ -117,3 +82,37 @@ function closeAllSelect(elmnt) {
 }
 
 document.addEventListener("click", closeAllSelect);
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('button') && event.target.matches('.modal-wrapper')) {
+        let modal = document.getElementsByClassName("modal");
+        let i;
+        for (i = 0; i < modal.length; i++) {
+            let openModal = modal[i];
+            if (openModal.classList.contains('show-modal')) {
+                openModal.classList.remove('show-modal');
+            }
+        }
+    }
+
+    if (!event.target.matches('button')) {
+        let dropdown = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdown.length; i++) {
+            let openDropdown = dropdown[i];
+            if (openDropdown.classList.contains('show-dropdown')) {
+                openDropdown.classList.remove('show-dropdown');
+            }
+        }
+    }
+}
+
+window.addEventListener('show-alert', function(event) {
+    let openAlert = document.getElementById(event.detail);
+    if(openAlert.classList.contains('show-alert')) {
+        setTimeout(function() {
+            document.getElementById(event.detail).classList.remove("show-alert");
+        }, 4000);
+    }
+});
--- teatime_db_mysql.sql
+++ teatime_db_mysql.sql
@@ -1,75 +1,71 @@
-
+/* 
+	BIKIN DULU DATABASENYA
+*/
 
 CREATE TABLE Pengguna
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	nama_pengguna varchar(255) NOT NULL,
 	tipe varchar(50) NOT NULL,
 	username varchar(255) NOT NULL,
 	password varchar(255) NOT NULL,
 	PRIMARY KEY (id),
 	UNIQUE (username)
-)
-
-CREATE TABLE TransaksiPemesanan
-(
-	id int NOT NULL AUTO_INCREMENT,
-	idKasir int NOT NULL,
-	nama_pemesan varchar(255) NOT NULL,
-	tanggal_transaksi date NOT NULL,
-	waktu_transaksi time NOT NULL,
-	idMenu int NOT NULL,
-	idToping int NOT NULL,
-	ukuran_gelas varchar(50) NOT NULL,
-	banyak_gula varchar(50) NOT NULL,
-	banyak_es varchar(50) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (idKasir) REFERENCES Pengguna(id)
-)
+);
 
 CREATE TABLE Menu
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	nama_minuman varchar(255) NOT NULL,
-	harga_regular int NOT NULL,
-	harga_large int NOT NULL,
+	harga_regular INT NOT NULL,
+	harga_large INT NOT NULL,
     PRIMARY KEY (id)
-)
-
-CREATE TABLE MemilikiMenu
-(
-	idTransaksi int NOT NULL,
-	idMenu int NOT NULL,
-    PRIMARY KEY (idTransaksi, idMenu),
-    FOREIGN KEY (idTransaksi) REFERENCES TransaksiPemesanan(id),
-	FOREIGN KEY (idMenu) REFERENCES Menu(id)
-)
+);
 
 CREATE TABLE Toping
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	nama_toping varchar(255) NOT NULL,
-	harga_toping int NOT NULL,
+	harga_toping INT NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE MemilikiToping
+CREATE TABLE TransaksiPemesanan
 (
-	idTransaksi int NOT NULL,
-	idToping int NOT NULL,
-    PRIMARY KEY (idTransaksi, idToping),
+	id INT NOT NULL AUTO_INCREMENT,
+	idKasir INT NOT NULL,
+	nama_pemesan varchar(255) NOT NULL,
+	tanggal_transaksi date NOT NULL,
+	waktu_transaksi time NOT NULL,
+	total INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idKasir) REFERENCES Pengguna(id)
+);
+
+CREATE TABLE DetailTransaksi
+(
+	idTransaksi INT NOT NULL,
+	idMenu INT NOT NULL,
+	idToping INT NOT NULL,
+	banyak_es VARCHAR(10) NOT NULL,
+	banyak_gula VARCHAR(10) NOT NULL,
+	ukuran_gelas VARCHAR(10) NOT NULL,
+    PRIMARY KEY (idTransaksi, IdMenu, IdToping),
     FOREIGN KEY (idTransaksi) REFERENCES TransaksiPemesanan(id),
+	FOREIGN KEY (idMenu) REFERENCES Menu(id),
 	FOREIGN KEY (idToping) REFERENCES Toping(id)
-)
+);
 
-
--- SEED PENGGUNA untuk coba login
--- username: frostygum, password: frostygum
+/* 	
+	SEED PENGGUNA untuk coba login
+	semua password: frostygum
+*/
 
 INSERT INTO `pengguna` (`id`, `nama_pengguna`, `tipe`, `username`, `password`) VALUES
-(26, 'juan', 'kasir', 'frostygum', '$2y$10$A/oDzRiF9qlI9ViNrNWyX.X4hOkq4QSD56xI0vkYgkYqZkpYjAKX6');
+(1, 'kasir_1', 'kasir', 'frostyKasir', '$2y$10$A/oDzRiF9qlI9ViNrNWyX.X4hOkq4QSD56xI0vkYgkYqZkpYjAKX6'),
+(2, 'admin_1', 'admin', 'frostyAdmin', '$2y$10$A/oDzRiF9qlI9ViNrNWyX.X4hOkq4QSD56xI0vkYgkYqZkpYjAKX6'),
+(3, 'manager_1', 'manager', 'frostyManager', '$2y$10$A/oDzRiF9qlI9ViNrNWyX.X4hOkq4QSD56xI0vkYgkYqZkpYjAKX6');
 
---SEED TOPPING
 INSERT INTO `toping` (`id`, `nama_toping`, `harga_toping`) VALUES
 (1, 'Pearl', 5000),
 (2, 'Pudding', 3000),
@@ -80,3 +76,45 @@ INSERT INTO `toping` (`id`, `nama_toping`, `harga_toping`) VALUES
 (7, 'Coffee Jelly', 4000),
 (8, 'Rainbow Jelly', 4000),
 (9, 'Read Beans', 4000);
+
+INSERT INTO `menu` (`id`, `nama_minuman`, `harga_regular`, `harga_large`) VALUES
+(1, 'Brown Sugar Milk Tea', 25000, 35000),
+(2, 'Roasted Milk Tea', 20000, 23000),
+(3, 'Brown Rice Green Milk Tea', 20000, 23000),
+(4, 'Jasmine Green Milk Tea', 20000, 23000),
+(5, 'Strawberry Milk Tea', 22000, 26000),
+(6, 'Vanilla Milk Tea', 22000, 25000),
+(7, 'Hazelnut Milk Tea', 22000, 25000),
+(8, 'Caramel Milk Tea', 22000, 27000),
+(9, 'Honey Milk Tea', 22000, 25000),
+(10, 'Taro Milk Tea', 23000, 27000),
+(11, 'Hazelnut Chocolate Milk Tea', 23000, 26000);
+
+INSERT INTO `transaksipemesanan` (`id`, `idKasir`, `nama_pemesan`, `tanggal_transaksi`, `waktu_transaksi`, `total`) VALUES
+(1, 1, 'tommy', '2020-05-08', '12:23:05', 60000);
+
+INSERT INTO `detailtransaksi` (`idTransaksi`, `idMenu`, `idToping`, `banyak_es`, `banyak_gula`, `ukuran_gelas`) VALUES
+(1, 1, 1, 'none', 'less', 'regular'),
+(1, 2, 1, 'none', 'less', 'large'),
+(1, 2, 2, 'none', 'less', 'large');
+
+/*
+	VIEWS
+*/
+
+CREATE VIEW transaksi AS
+SELECT 
+	transaksipemesanan.id,
+	nama_pemesan,
+    nama_minuman,
+    nama_toping,
+    ukuran_gelas,
+    banyak_es,
+    banyak_gula,
+    waktu_transaksi,
+    tanggal_transaksi,
+	total
+FROM 
+	detailtransaksi 
+    JOIN transaksipemesanan ON transaksipemesanan.id = detailtransaksi.idTransaksi 
+    JOIN menu ON menu.id = detailtransaksi.idMenu JOIN toping ON toping.id = detailtransaksi.idToping 
